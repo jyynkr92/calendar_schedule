a335d63fda3b9f40db2694cca146cc3320be64c4
aliceyoo <jyynkr92@gmail.com>
2019-10-30 15:50:43 +0900

    empty config key

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,6 @@
 import firebase from "firebase";
 
-const config = {
-  apiKey: "AIzaSyB5zZiB86trnimmAFdt1Y3D2y7-L1WqhzU",
-  authDomain: "forestellaschedule.firebaseapp.com",
-  databaseURL: "https://forestellaschedule.firebaseio.com",
-  projectId: "forestellaschedule",
-  storageBucket: "forestellaschedule.appspot.com",
-  messagingSenderId: "915810306801",
-  appId: "1:915810306801:web:7d37f8164d4e15526e7750",
-  measurementId: "G-J1VJ13S7SY"
-};
+const config = {};
 firebase.initializeApp(config);
 const firestore = firebase.firestore();
 

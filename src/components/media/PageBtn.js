9cdb6bfe865c510490f5f03fd7ce2e790e603b25
aliceyoo <jyynkr92@gmail.com>
2019-12-02 21:31:48 +0900

    styled and add function of pagination

---
+++ src/components/media/PageBtn.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+
+const PageBtn = ({ curPage, movePage, value }) => {
+  return (
+    <PageSpan
+      curPage={curPage}
+      value={value}
+      onClick={() => {
+        movePage(value);
+      }}
+    >
+      {value}
+    </PageSpan>
+  );
+};
+
+const PageSpan = styled.div`
+  background-color: ${props => (props.curPage === props.value ? `#d9b99b` : `#e4d5b7`)};
+  display: inline-block;
+  border: 1px solid #d9d9d9;
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  text-align: center;
+  line-height: 36px;
+  margin: 0px 5px;
+  cursor: pointer;
+`;
+
+export default PageBtn;
--- src/components/media/Pagination.js
+++ src/components/media/Pagination.js
@@ -1,27 +1,45 @@
 import React from "react";
+import PageBtn from "./PageBtn";
+import styled from "styled-components";
 
 const Pagination = ({ moveOnePage, movePage, curPage }) => {
   return (
-    <div>
-      <span
+    <PaginationArea>
+      <PageSpan
         onClick={() => {
           moveOnePage(-1);
         }}
       >
-        previous
-      </span>
-      <span>1</span> <span>2</span>
-      <span>3</span> <span>4</span> <span>5</span> <span>6</span> <span>7</span> <span>8</span>
-      <span>9</span> <span>10</span>
-      <span
+        &lt;
+      </PageSpan>
+      {Array.from(Array(10), (e, i) => {
+        return <PageBtn curPage={curPage} movePage={movePage} value={i + 1} />;
+      })}
+      <PageSpan
         onClick={() => {
           moveOnePage(1);
         }}
       >
-        next
-      </span>
-    </div>
+        &gt;
+      </PageSpan>
+    </PaginationArea>
   );
 };
+
+const PaginationArea = styled.div`
+  text-align: center;
+`;
+
+const PageSpan = styled.div`
+  display: inline-block;
+  border: 1px solid #d9d9d9;
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  text-align: center;
+  line-height: 36px;
+  margin: 0px 5px;
+  cursor: pointer;
+`;
 
 export default Pagination;
